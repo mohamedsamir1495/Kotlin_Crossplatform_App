520dbe83e76078a03a9c6f4bb57465a61ab78fe7
Mohamed Samir <mohamedsamir1495@gmail.com>
2018-12-11 17:10:27 +0200

    - First Commit

---
+++ SharedCode/src/iosMain/kotlin/actual.kt
@@ -0,0 +1,10 @@
+package org.kotlin.mpp.mobile
+
+import platform.UIKit.UIDevice
+
+actual fun platformName(): String {
+
+    return UIDevice.currentDevice.systemName() +
+            " " +
+            UIDevice.currentDevice.systemVersion
+}
